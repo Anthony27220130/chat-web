7bdc8df4b58484563c651467f2401416c4cb44f9
Julian <vega.juliann.27@gmail.com>
2025-05-10 10:22:54 -0500

    1er commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  base_url: 'http://ec2-18-221-54-128.us-east-2.compute.amazonaws.com:8080' // Reemplaza con la URL de tu EC2
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environments = {
+  base_url: 'http://ec2-18-221-54-128.us-east-2.compute.amazonaws.com:8080' // URL pública de tu instancia EC2
+};
